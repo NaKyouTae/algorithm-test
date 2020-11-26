44f570297c8624723899d8764ec0b1a7c36c06cc
나규태 <kyoutae@ybtour.co.kr>
2020-11-26 13:31:39 +0900

    two day

---
+++ src/test/java/algorithm/test/P4.java
@@ -0,0 +1,108 @@
+package algorithm.test;
+
+import org.junit.Test;
+
+public class P4 {
+	@Test
+	public void test() {
+		int num = 5;
+		
+		triangle1(num);
+		System.out.println();
+		triangle2(num);
+		System.out.println();
+		triangle3(num);
+		System.out.println();
+		triangle4(num);
+		System.out.println();
+		triangleCenter1(num);
+		System.out.println();
+		triangleCenter2(num);
+		System.out.println();
+		triangleNumber(num);
+	}
+	
+	public void triangle1(int n) {
+		for(int i = 1; i<=n; i++) {
+			for(int j = 1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	public void triangle2(int n) {
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<n-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void triangle3(int n) {
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<n; j++) {
+				if(j < i) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	public void triangle4(int n) {
+		for(int i = 0; i<n; i++) {
+			for(int j = 0; j<n; j++) {
+				if(n-i <= j+1) {
+					System.out.print("*");
+				}else {					
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	public void triangleCenter1(int n) {
+		for(int i = 0; i<n; i++) {
+			for(int j = 1; j < n - i; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 0; k < i * 2 + 1; k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+	}
+	
+	public void triangleCenter2(int n) {
+		for(int i = n; i>0; i--) {
+			for(int j = n - i; j > 0; j--) {
+				System.out.print(" ");
+			}
+			for(int k = i * 2 - 1; k > 0 ; k--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void triangleNumber(int n) {
+		for(int i = 0; i<n; i++) {
+			for(int j = 1; j < n - i; j++) {
+				System.out.print(" ");
+			}
+			for(int k = 0; k < i * 2 + 1; k++) {
+				System.out.print(i+1);
+			}
+			
+			System.out.println();
+		}
+	}
+	
+}
