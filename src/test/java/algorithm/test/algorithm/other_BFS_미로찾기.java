7db1051b1220bfbd0231a5808132f9e9abbc2018
nakyoutae <qppk@naver.com>
2021-03-04 21:54:01 +0900

    ㅁㅇㅇ

--- src/test/java/algorithm/test/algorithm/other_BFS_미로찾기.java
+++ src/test/java/algorithm/test/algorithm/other_BFS_미로찾기.java
@@ -1,67 +1,59 @@
 package algorithm.test.algorithm;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class other_BFS_미로찾기 {
-	/******************************************************************************
+    static class Node{
+        int r;
+        int c;
+        Node(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
 
-	Welcome to GDB Online.
-	GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-	C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-	Code, Compile, Run and Debug online from anywhere in world.
+        int[][] map = new int[N][M];
+        int[][] check = new int[N][M];
 
-	*******************************************************************************/
-	import java.util.*;
-	public class Main
-	{
-	    static class Node{
-	        int r;
-	        int c;
-	        Node(int r, int c){
-	            this.r = r;
-	            this.c = c;
-	        }
-	    }
-	   public static void main(String[] args) {
-	        Scanner sc = new Scanner(System.in);
-	        int N = sc.nextInt();
-	        int M = sc.nextInt();
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                map[i][j] = sc.nextInt();
+            }
+        }
 
-	        int[][] map = new int[N][M];
-	        int[][] check = new int[N][M];
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(0,0));
+        check[0][0] = 1;
 
-	        for(int i=0; i<N; i++){
-	            for(int j=0; j<M; j++){
-	                map[i][j] = sc.nextInt();
-	            }
-	        }
+        int[] dr = {1, -1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
 
-	        Queue<Node> q = new LinkedList<>();
-	        q.add(new Node(0,0));
-	        check[0][0] = 1;
+        while(!q.isEmpty()){
+            Node buf = q.poll();
 
-	        int[] dr = {1, -1, 0, 0};
-	        int[] dc = {0, 0, -1, 1};
+            for(int d=0; d<4; d++){
+                int r = buf.r + dr[d];
+                int c = buf.c + dc[d];
 
-	        while(!q.isEmpty()){
-	            Node buf = q.poll();
+                if(r<0 || c<0 || r>=N||c>=M)
+                    continue;
+                
+                if(map[r][c] == 1 && check[r][c] == 0){
+                    check[r][c] = check[buf.r][buf.c] + 1;
+                    q.add(new Node(r,c));
+                }
+            }
+        }
 
-	            for(int d=0; d<4; d++){
-	                int r = buf.r + dr[d];
-	                int c = buf.c + dc[d];
-
-	                if(r<0 || c<0 || r>=N||c>=M)
-	                    continue;
-	                
-	                if(map[r][c] == 1 && check[r][c] == 0){
-	                    check[r][c] = check[buf.r][buf.c] + 1;
-	                    q.add(new Node(r,c));
-	                }
-	            }
-	        }
-
-	        System.out.println(check[N-1][M-1]);
+        System.out.println(check[N-1][M-1]);
 
 
 
-	   }
-	}
+   }
 }
