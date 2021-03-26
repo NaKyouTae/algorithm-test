251e8727a3fed9d5d56522d66fd9723855796cf9
nakyoutae <qppk@naver.com>
2021-03-26 13:42:12 +0900

    소년점프 ing

---
+++ src/test/java/algorithm/test/study/강의실배정_복습.java
@@ -0,0 +1,71 @@
+package algorithm.test.study;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+//3
+//1 3
+//2 4
+//3 5
+
+//https://www.acmicpc.net/problem/11000
+
+public class 강의실배정_복습 {
+	public static class Node {
+		int x, y;
+		Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	public static Comparator<Node> compare = new Comparator<>() {
+
+		@Override
+		public int compare(Node o1, Node o2) {
+			return (o1.x == o2.x) ? 0:(o1.x > o2.x) ? 1:-1;
+		}
+	};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int l = Integer.parseInt(br.readLine());
+		
+		List<Node> list = new ArrayList<>();
+		for (int i = 0; i < l; i++) {
+			String[] str = br.readLine().split(" ");
+			int s = Integer.parseInt(str[0]);
+			int e = Integer.parseInt(str[1]);
+			
+			list.add(new Node(s, e));
+		}
+		
+		Collections.sort(list, compare);
+		
+		PriorityQueue<Integer> que = new PriorityQueue<>();
+		list.forEach(item -> {
+			if(que.isEmpty()) {
+				que.add(item.y);
+			}else {
+				if(que.peek() <= item.x) {
+					que.poll();
+					que.offer(item.y);
+				}else {
+					que.add(item.y);
+				}
+			}
+		});
+		
+		bw.write(Integer.toString(que.size()));
+		bw.flush();
+		bw.close();
+	}
+}
--- src/test/java/algorithm/test/study/소년점프.java
+++ src/test/java/algorithm/test/study/소년점프.java
@@ -5,21 +5,96 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
+//5 5
+//00011
+//01011
+//00110
+//00001
+//10100
+//1 1
+//5 5
+//5 2
 
 //https://www.acmicpc.net/problem/16469
 public class 소년점프 {
+	public static int[][] arr, brr;
+	public static boolean[][] visit;
+	public static int X, Y;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        
-        bw.write();
+		
+		String[] str = br.readLine().split(" ");
+		
+		X = Integer.parseInt(str[0]);
+		Y = Integer.parseInt(str[1]);
+		
+		arr = new int[X][Y];
+		brr = new int[X][Y];
+		visit = new boolean[X][Y];
+		
+		for(int i = 0; i < X; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < Y; j++) {
+				arr[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));
+			}
+		}
+		
+		for(int i = 0; i < 3; i++) {
+			String[] location = br.readLine().split(" ");
+			int n = Integer.parseInt(location[0])-1;
+			int m = Integer.parseInt(location[1])-1;
+			visit[n][m] = true;
+			dfs(n, m);
+		}
+		
+		int min = Integer.MAX_VALUE;
+		
+		for (int i = 0; i < X; i++) {
+			for (int j = 0; j < Y; j++) {
+				int val = brr[i][j];
+				if(val < min && val != 0) {
+					min = val;
+				}
+			}
+		}
+		
+		int cnt = 0;
+		
+		for (int i = 0; i < X; i++) {
+			for (int j = 0; j < Y; j++) {
+				int val = brr[i][j];
+				if(val == min) {
+					cnt++;
+				}
+			}
+		}
+		
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(min).append("\n");
+		sb.append(cnt);
+		
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
+	}
+	
+	public static void dfs(int n, int m) {
+		int[] dx = {0, -1, 0, 1};
+		int[] dy = {-1, 0, 1, 0};
+		
+		for(int i = 0; i < 4; i++) {
+			int xx = n + dx[i], yy = m + dy[i];
+			if(xx < X && yy < Y && xx >= 0 && yy >= 0) {
+				if(!visit[xx][yy] && arr[xx][yy] != 1) {
+					visit[xx][yy] = true;
+					brr[xx][yy]++;
+					dfs(xx, yy);
+				}
+			}
+		}
 	}
 }
